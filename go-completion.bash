1983c5250c7407e53feb288e1d5c0cc21a1e176e
Makoto Onuki <omakoto@google.com>
2015-05-20 18:09:56 -0700

    Add self update function

--- go-completion.bash
+++ go-completion.bash
@@ -2,17 +2,23 @@
 #
 # Usage: source go-completion.bash
 
-function _go_complete() {
+# Update this script.
+go_complete_update() {
+  curl "https://raw.githubusercontent.com/omakoto/go-completion.bash/master/go-completion.bash" > \
+      ${BASH_SOURCE[0]}
+}
+
+_go_complete() {
   local -A _go_flags
   local _go_build_flags="-a -n -p -race -v -work -x -cclfags -compiler -gccgoflags -gcflags -installsuffix -ldflags -tags"
   local _go_test_flags="-bench -benchmem -benchtime -blockprofile -blockprofilerate -cover -covermode -coverpkg -coverprofile -cpu -cpuprofile -memprofile -memprofilerate -outputdir -parallel -run -short -timeout -v"
 
-  cmd="${COMP_WORDS[0]}"
-  sub="${COMP_WORDS[1]}"
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  local cmd="${COMP_WORDS[0]}"
+  local sub="${COMP_WORDS[1]}"
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  cand=""
+  local cand=""
   case "$prev" in
     "go")
       cand="build clean env fix fmt get install list run test tool version vet"
@@ -44,10 +50,10 @@ function _go_complete() {
   fi
 }
 
-function _godoc_complete() {
-  cur="${COMP_WORDS[COMP_CWORD]}"
+_godoc_complete() {
+  local cur="${COMP_WORDS[COMP_CWORD]}"
 
-  cand=""
+  local cand=""
   case "$cur" in
     -*)
       cand="-v -q -src -tabwidth -timestamps -index -index_files -index_throttle -links -write_index -index_files -maxresults -notes -html -goroot -http -server -analysis -templates -url -zip"
@@ -62,3 +68,5 @@ function _godoc_complete() {
 
 complete -o filenames -o bashdefault -F _go_complete go
 complete -o filenames -o bashdefault -F _godoc_complete godoc
+
+echo OK
